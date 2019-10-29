d7eadb04d2409ddcc6bb509b5919c923e0f8f0ee
sbedene <sylvainbedene@gmail.com>
2019-10-29 18:57:29 +0100

    start unit tests

--- unit test/test.c
+++ unit test/test.c
@@ -1,37 +1,38 @@
 #include <stdlib.h>
 #include <ud_math.h>
 
-
-int main(int ac, char **av)
+int prec(float val, float example)
 {
-    // ud_arr  *nb_a = ud_arr_set(float, 10, 1,2,3,4,5,6,7,8,9,10);
+    printf("%f\n%f\n\n", val, example);
+    float   prec = 0.0001;
+    float diff = val - example;
+    return (diff < prec && diff > -prec);
+}
 
-    // ud_ut_time("%s", "Starting tests...");
-    // //Basic tests
-    // assert(ud_math_sqrt(25) == 5);
-    // assert(ud_math_pow(10, 2) == 100);
-    // assert(!ud_str_cmp(ud_math_itoa(4354), ud_arr_set(char, 4, '4', '3', '5', '4')));
-    // assert(ud_math_mean(nb_a) == 5.5);
-    // assert(ud_math_rand(0, 10) != ud_math_rand(0, 10));
-    // assert(ud_math_atof("42.101") - 42.101f < 0.01);
-    // assert(ud_math_atof("-42.101") + 42.101f < 0.01);
-    // assert(ud_math_atof("0") - 0 < 0.01);
-    // assert(ud_math_atof("257272.5727425752") - 257272.5727425752f < 0.01);
-    // ud_ut_time("%s", "All tests passed.");
-
-    // printf("%s\n", ud_math_itoa(56));
-    // printf("%s\n", ud_math_itoa(-1));
-    // printf("%s\n", ud_math_itoa(0));
-    // printf("%s\n", ud_math_itoa(9));
-    // printf("%s\n", ud_math_itoa(1));
-    // printf("%s\n", ud_math_itoa(2147483647));
-    // printf("%s\n", ud_math_itoa(-2147483648));
+int main(void)
+{
+    char    *one = "1";
+    char    *two = "2";
+    char    *dec1 = "0.1";
+    char    *dec1000 = "0.0001";
+    char    *dec2 = "0.00000000000001";
+    char    *dec3 = "0.214";
+    char    *dec4 = "0.124129014832948239041241312";
+    char    *int_dec = "1.2";
+    char    *int_dec2 = "2134123.2351351345431";
+    char    *big = "1294302481254901357134905.5314593405134905134513451";
     
-    // size_t tata[] = {5, 3, 4, 9, 0, 1};
-    // ud_math_si_sort(tata, 6);
-    // for (size_t i = 0; i < 6; ++i)
-    // {
-    //     printf("%zd\n", tata[i]);
-    // }
-    printf("%s\n", ud_math_ftoa_prec(0.5f, 5));
+    ud_ut_test(prec(ud_math_atof(one), 1));
+    ud_ut_test(prec(ud_math_atof(two), 2));
+    ud_ut_test(prec(ud_math_atof(dec1), 0.1));
+    ud_ut_test(prec(ud_math_atof(dec1000), 0.0001));
+    ud_ut_test(prec(ud_math_atof(dec2), 0.00000000000001));
+    ud_ut_test(prec(ud_math_atof(dec3), 0.214));
+    ud_ut_test(prec(ud_math_atof(dec4), 0.124129014832948239041241312));
+    ud_ut_test(prec(ud_math_atof(int_dec), 1.2));
+    ud_ut_test(prec(ud_math_atof(int_dec2), 2134123.2351351345431));
+    ud_ut_test(prec(ud_math_atof(big), 1294302481254901357134905.5314593405134905134513451));
+    ud_ut_test(prec(exp(0), 1));
+    ud_ut_test(prec(exp(1), 2.7182));
+    ud_ut_test(prec(exp(23.21), 12021950210.2));
 }
