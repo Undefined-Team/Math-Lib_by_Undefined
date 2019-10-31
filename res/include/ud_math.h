74a54dfd25a17f4fba11030855001573e569cb39
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-31 16:38:31 +0100

    add _ to macro variables

--- res/include/ud_math.h
+++ res/include/ud_math.h
@@ -7,20 +7,20 @@
 #include <ud_string.h>
 
 // Macro
-# define    ud_math_max(x, y)               (x > y ? x : y)
-# define    ud_math_min(x, y)               (x < y ? x : y)
-# define    ud_math_is_digit(c)             ((c) >= '0' && (c) <= '9')
-# define    ud_math_is_white_space(c)       ((c) == ' ' || (c) == '\t')
+# define    ud_math_max(_x, _y)             ({ __auto_type _xt = _x; __auto_type _yt = _y; (_xt > (typeof(_xt))_yt ? _xt : (typeof(_xt))_yt); })
+# define    ud_math_min(_x, _y)             ({ __auto_type _xt = _x; __auto_type _yt = _y; (_xt < (typeof(_xt))_yt ? _xt : (typeof(_xt))_yt); })
+# define    ud_math_is_digit(_c)            ({ __auto_type _ct = _c; ((_ct) >= '0' && (_ct) <= '9'); })
+# define    ud_math_is_white_space(_c)      ({ __auto_type _ct = _c; ((_ct) == ' ' || (_ct) == '\t'); })
 
-# define    ud_math_sigmoid(x)              (1 / (1 + ud_math_exp(-x)))
-# define    ud_math_relu(x)                 ud_math_max(0, x)
-# define    ud_math_lrelu(x)                ud_math_max(0.1 * x, x)
-# define    ud_math_tanh(x)                 (1 - (2 / (ud_math_exp(2 * x) + 1)))
+# define    ud_math_sigmoid(_x)             (1 / (1 + ud_math_exp(-_x)))
+# define    ud_math_relu(_x)                ud_math_max(0, _x)
+# define    ud_math_lrelu(_x)               ({ __auto_type _xt = _x; ud_math_max(0.1 * _xt, _xt); })
+# define    ud_math_tanh(_x)                (1 - (2 / (ud_math_exp(2 * _x) + 1)))
 
-# define    ud_math_si_sort(arr, len)       ud_math_merge_sort((size_t*)arr, 0, len - 1)
-# define    ud_math_itoa(x)                 ud_math_itoa_l(x, NULL)
+# define    ud_math_si_sort(_arr, _len)     ud_math_merge_sort((size_t*)_arr, 0, _len - 1)
+# define    ud_math_itoa(_x)                ud_math_itoa_l(_x, NULL)
 
-# define    ud_math_ftoa(n)                 ud_math_ftoa_prec(n, 5)
+# define    ud_math_ftoa(_n)                ud_math_ftoa_prec(_n, 5)
 
 // Structures
 
